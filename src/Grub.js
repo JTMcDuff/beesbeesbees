ed005a833c7164e5a8191eb138a890f95088447c
MKS-students <mks.students@hackreactor.com>
2016-10-31 13:32:16 -0500

    Done with Bees.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
 };
 
+var grub = new Grub();
+Grub.prototype.constructor = Grub;
+Grub.prototype.eat = function () {
+
+};
